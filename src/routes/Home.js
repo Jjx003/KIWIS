1073b04e39b2ac1a7c414ff0c4149d26cb76ba50
Aidan Denlinger <adenling@ucsd.edu>
2020-04-28 14:17:42 -0700

    carrying over work from last repo: very basic navbar

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Menu, Input, Dropdown } from 'semantic-ui-react';
 import '../css/App.css';
 
 class Home extends React.Component {
 	render() {
 		return(
 			<div>
+        <Menu fluid widths={3}>
+          <Menu.Item name='KIWI'>KIWI</Menu.Item>
+
+         <Menu.Menu position='center'>
+            <Input placeholder="What's your question?" />
+          </Menu.Menu>
+
+            <Dropdown item text='Tags'>
+              <Dropdown.Menu>
+                <Dropdown.Item>C</Dropdown.Item>
+                <Dropdown.Item>that's it.</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+        </Menu>
 				<h1> Home Page </h1>	
 				<Button> Hi </Button>
 			</div>
