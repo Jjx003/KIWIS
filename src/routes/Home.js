9ba0d005e719fee04b950b890ac485a53cec3c4b
Aidan Denlinger <adenling@ucsd.edu>
2020-04-28 15:56:20 -0700

    forgot profile picture. started working on flex with search bar and tags

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Menu, Input, Dropdown, Image, Checkbox, List } from 'semantic-ui-react';
+import { Menu, Input, Dropdown, Image, List, Icon } from 'semantic-ui-react';
 import '../css/App.css';
 import logo from '../images/logo_white.png';
 
@@ -14,16 +14,22 @@ class Home extends React.Component {
 	render() {
 		return(
 			<div>
-        <Menu fluid size='large' color='olive' inverted widths={3} className="navbar">
+        <Menu secondary size='large' color='olive' inverted className="navbar">
           <Menu.Item name='KIWI'>
               <Image fluid size='tiny' src={logo}/>
           </Menu.Item>
 
-         <Menu.Item>
+         <Menu.Item style={{flexGrow: 2}}>
             <Input placeholder="What's your question?" />
           </Menu.Item>
 
-          <Dropdown fluid multiple selection item options={options} text='Tags' />
+          <Menu.Item fitted>
+            <Dropdown multiple selection item options={options} text='Tags' />
+          </Menu.Item>
+
+          <Menu.Item name='options'>
+            <Icon name="user" />
+          </Menu.Item>
         </Menu>
         <List divided relaxed className="questions">
         <List.Item>
