11485fa6240f161374e3ec490851a66dd9fa0293
joecai6 <55220898+joecai6@users.noreply.github.com>
2020-04-28 15:31:21 -0700

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './css/App.css';
 
 import Home from './routes/Home';
 
 function App() {
   return (
-    <div className="App">
-	 	<Route path="/" exact render={(props) => <Home {...props}/>} />
-    </div>
+    <BrowserRouter>
+			<div className="App">
+	 			<Route path="/" exact render={(props) => <Home {...props}/>} />
+    	</div>
+  	</BrowserRouter>
   );
 }
 
