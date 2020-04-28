5bdcfe199d914839dc2c0ec25fc8266323f0a5c6
Aidan Denlinger <adenling@ucsd.edu>
2020-04-28 15:34:46 -0700

    fully implemented v1 navbar + basic idea of what questions can look like?

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { Button, Menu, Input, Dropdown } from 'semantic-ui-react';
+import { Button, Menu, Input, Dropdown, Image, Checkbox, List } from 'semantic-ui-react';
 import '../css/App.css';
+import logo from '../images/logo_white.png';
+
+const options = [
+  { key: 'c', text: 'C', value: 'c' },
+  { key: 'c++', text: 'C++', value: 'c++' },
+  { key: 'c++++', text: 'C++++', value: 'c++++' },
+  { key: 'c--', text: 'C--', value: 'c--' },
+]
 
 class Home extends React.Component {
 	render() {
 		return(
 			<div>
         <Menu fluid size='large' color='olive' inverted widths={3} className="navbar">
-          <Menu.Item name='KIWI'>KIWI</Menu.Item>
+          <Menu.Item name='KIWI'>
+              <Image fluid size='tiny' src={logo}/>
+          </Menu.Item>
 
          <Menu.Item>
             <Input placeholder="What's your question?" />
           </Menu.Item>
 
-          <Dropdown item text='Tags'>
-            <Dropdown.Menu>
-              <Dropdown.Item>C</Dropdown.Item>
-              <Dropdown.Item>that's it.</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <Dropdown fluid multiple selection item options={options} text='Tags' />
         </Menu>
-				<h1> Home Page </h1>	
-				<Button> Hi </Button>
+        <List divided relaxed className="questions">
+        <List.Item>
+        <List.Content>
+          <List.Header>Ok, I did my hello world, how do I goodbye world?</List.Header>
+          <List.Description>tags:C</List.Description>
+        </List.Content>
+        </List.Item>
+        <List.Item>
+        <List.Content>
+          <List.Header>Trying to do Hello World and C is complaining that hello world isn't a command?</List.Header>
+          <List.Description>tags:C</List.Description>
+        </List.Content>
+        </List.Item>
+        </List>
 			</div>
 		);
 	}
