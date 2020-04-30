d8e2302888edbc22990f2f859401780b02355e7c
Jason Cabrera <cabrerajason7@gmail.com>
2020-04-30 02:20:19 -0700

    updated server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
 var inviteRouter = require('./routes/invite');
+app.use('/inviteUser', inviteRouter);
+app.use(express.static('../src/routes'));
+app.use(express.static(path.join(__dirname, '../build')));
 
 const port = 9000;
 app.get('/', (req, res) => res.send("hello world"));
--- server/routes/invite.js
+++ server/routes/invite.js
@@ -6,15 +6,20 @@ router.post('/', function (req, res, next) {
     nodemailer 
     localhost9000/invite/uid
     */
+   
 });
 
-router.post('/accept_invite/:uuid', function(req, res, next) {
+router.get('/accept_invite/:uuid', function(req, res, next) {
+    console.log("page found");
     /*
     check the db if valid uid
         redirect to sign up page
-
+        res.redirect(localhost:3000/user);
         // mark uid as invalid at the end of the sign up method
         // leave confirmation of email to firebase code 
     */
+   res.sendFile('index.html', {root: '../build/'});
 });
+
+module.exports = router;
 
