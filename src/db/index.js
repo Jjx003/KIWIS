e666f07a0416d8dabf495585c9be2f493cd92414
Eric Siu <esiu@ucsd.edu>
2020-04-30 02:27:19 -0700

    added my get/post and tested for tags

--- src/db/index.js
+++ src/db/index.js
@@ -13,5 +13,65 @@ const db = firebase.initializeApp({
 
 // add database functions below
 
+// NOTE (Eric): in order to get userId: firebase.auth().currentUser.uid
+// Also, forumDBRef requires the forumName so they can access the specific company
 
-export default db;
+// POST method for new users
+function createNewUser(forumName, firstName, lastName, email) {
+    const forumDBRef = db.database().ref(forumName);
+    var user = {};
+    var userID = firebase.auth().currentUser.uid;
+
+    // Creates a new user object with the userID as a key
+    user[userID] = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        admin: false
+    };
+	forumDBRef.child("Users").push(user);
+}
+
+// GET method for users (NEED TO FIX or make better lol)
+function getUser(forumName, userID) {
+    const forumDBRef = db.database().ref(forumName);
+    var user = {};
+	forumDBRef.child('Users/' + userID).once('value', function(data) {
+        data.forEach(function(item) {
+            user[item.key] = item.val();
+        });
+    });
+}
+
+// POST method for new tags
+function createNewTag(forumName, tagName) {
+    const forumDBRef = db.database().ref(forumName);
+    var tag = {};
+    tag[tagName] = {count: 0}
+    forumDBRef.child("Tags").set(tag);
+}
+
+// GET method for tags in forums (NEED TO FIX or make better lol)
+function getTags(forumName) {
+    const forumDBRef = db.database().ref(forumName);
+    var tags = {};
+	forumDBRef.child('Tags').once('value', function(data) {
+        data.forEach(function(item) {
+            tags[item.key] = item.val();
+        });
+    });
+    return tags;
+}
+
+// GET method for a tag's number (NEED TO TEST)
+function getTagCount(forumName, tagName) {
+    const forumDBRef = db.database().ref(forumName);
+    var count = -1;
+	forumDBRef.child('Tags/' + tagName).once('value', function(data) {
+        count = data['count'];
+    });
+
+    return count;
+}
+
+export default {db, createNewUser, getUser, createNewTag, getTags, getTagCount};
