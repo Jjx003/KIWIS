0974cc0766f821a858d0b9c2f0cee854bdefb63d
Aidan Denlinger <adenling@ucsd.edu>
2020-04-30 20:35:17 -0700

    fixed tag selection overflow, moved Navbar as a component

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Menu, Input, Dropdown, Image, List, Icon, Grid, Button } from 'semantic-ui-react';
+import '../css/App.css';
+import logo from '../images/logo_white.png';
+import tags from '../dummy_data/dummy_data_navbar.json'
+
+const options = Object.keys(tags).map(x => { return { key: x, text: x, value: x } })
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <Menu secondary size='massive' color='olive' inverted className="navbar">
+                    <Menu.Item name='KIWI'>
+                        <Image fluid size='tiny' src={logo} />
+                    </Menu.Item>
+
+                    <Menu.Item fitted style={{ flexGrow: 2 }}>
+                        <Grid verticalAlign="middle" style={{ flexGrow: 2 }} columns={2}>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <Input fluid placeholder="What's your question?" />
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Dropdown fluid multiple selection options={options} text='Tags' />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Menu.Item>
+
+                    <Menu.Item name='options'>
+                        <Icon name="user" />
+                    </Menu.Item>
+
+                    <Menu.Item name='sign-out'>
+                        <Button>Sign Out</Button>
+                    </Menu.Item>
+                </Menu>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,54 +1,11 @@
 import React from 'react';
-import { Menu, Input, Dropdown, Image, List, Icon, Grid } from 'semantic-ui-react';
-import '../css/App.css';
-import logo from '../images/logo_white.png';
-import tags from '../dummy_data/dummy_data_navbar.json'
-
-const options = Object.keys(tags).map(x => { return { key: x, text: x, value: x } })
+import Navbar from '../components/Navbar'
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Menu secondary size='massive' color='olive' inverted className="navbar">
-          <Menu.Item name='KIWI'>
-            <Image fluid size='tiny' src={logo} />
-          </Menu.Item>
-
-          <Menu.Item fitted style={{ flexGrow: 2 }}>
-            <Grid verticalAlign="middle" style={{ flexGrow: 2 }} columns={2}>
-              <Grid.Row>
-                <Grid.Column>
-                  <Input fluid placeholder="What's your question?" />
-                </Grid.Column>
-                <Grid.Column>
-                  <Dropdown fluid multiple selection item options={options} text='Tags' />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Menu.Item>
-
-          <Menu.Item fitted>
-          </Menu.Item>
-
-          <Menu.Item name='options'>
-            <Icon name="user" />
-          </Menu.Item>
-        </Menu>
-        <List divided relaxed className="questions">
-          <List.Item>
-            <List.Content>
-              <List.Header>Ok, I did my hello world, how do I goodbye world?</List.Header>
-              <List.Description>tags:C</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>Trying to do Hello World and C is complaining that hello world isn't a command?</List.Header>
-              <List.Description>tags:C</List.Description>
-            </List.Content>
-          </List.Item>
-        </List>
+        <Navbar />
       </div>
     );
   }
