9dc66bd109d5839a9ba7305396da5897688e4634
Aidan Denlinger <adenling@ucsd.edu>
2020-04-30 21:00:54 -0700

    remove App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import './css/App.css';
 
 import Home from './routes/Home';
 
 function App() {
   return (
     <div className="App">
-	 	<Route path="/" exact render={(props) => <Home {...props}/>} />
+      <Route path="/" exact render={(props) => <Home {...props} />} />
     </div>
   );
 }
