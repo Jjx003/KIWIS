5b9cda81895bfb8c2862d41988a3768215f19eda
Riley Paik <ripaik@ucsc.edu>
2020-05-01 14:34:59 -0700

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -3,39 +3,13 @@ import { Route } from 'react-router-dom';
 import './css/App.css';
 
 import Home from './routes/Home';
-import db from './db/index.js';
-
-// import Posts from './components/Posts';
-// import DisplayData from './components/DisplayData';
-
 
 function App() {
-
   return (
-  <div>
-    
-  </div>
-
-  );
-
-/*  return (
     <div className="App">
 	 	<Route path="/" exact render={(props) => <Home {...props}/>} />
     </div>
   );
-  */
 }
-
-
-/*
-function App() {
-  return (
-    <div className="App">
-	 	<Route path="/" exact render={(props) => <Posts {...props}/>} />
-     <Route path="/" exact render={(props) => <DisplayData {...props}/>} />
-    </div>
-  );
-}
-*/
 
 export default App;
--- src/db/index.js
+++ src/db/index.js
@@ -15,10 +15,10 @@ const db = firebase.initializeApp({
 // add database functions below
 
 // Print the values of the post linked to the inputted key
-function printPost(post_id) {
+function printPost(forumName, post_id) {
 
-    // Reference the company's firebase "bruh" and the post's key
-    const firebaseRef = db.database().ref("bruh"+"/"+post_id);
+    // Reference the company's firebase and the post's key
+    const firebaseRef = db.database().ref(forumName+"/"+post_id);
 
     // Get a snapshot of the post
     firebaseRef.on("value", function(snapshot) {
@@ -41,17 +41,10 @@ function printPost(post_id) {
 
 }
 
-// Print the values of the post that is in the database
-var post_id = "-M67VDK6MLzICpgZPPPr";
-printPost(post_id);
+function addPostData(forumName, p_user_id, p_title, p_tag_ids, p_date_time, p_content) {
 
-// ---------------------------------------------------------------------------------------------
-
-
-function addPostData(p_user_id, p_title, p_tag_ids, p_date_time, p_content) {
-
-    // Reference the company's firebase "bruh"
-    const firebaseRef = db.database().ref("bruh");
+    // Reference the company's firebase
+    const firebaseRef = db.database().ref(forumName);
 
     // Push data inputted to firebase and also store reference of the push in "post_reference"
     var post_reference = firebaseRef.push({user_id: p_user_id, 
@@ -72,14 +65,5 @@ function addPostData(p_user_id, p_title, p_tag_ids, p_date_time, p_content) {
 
 }
 
-
-// Making test variables to pass into addPostData()
-var p_user_id = 1234, p_title = "this post's title", p_tag_ids = ["Node.js", "React"];
-var p_date_time = "4/29/20 at 5:00PM", p_content = "this post's content";
-
-// Call addPostData() using the variables above
-addPostData(p_user_id, p_title, p_tag_ids, p_date_time, p_content);
-
-
-export default {addPostData, printPost, db};
+export default {db, addPostData, printPost};
 
