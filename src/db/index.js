17f70ad7604679a35676fd88eea069ecf02cf24d
joecai6 <55220898+joecai6@users.noreply.github.com>
2020-05-04 16:42:31 -0700

    pushed data into algolia

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,11 @@
 import * as firebase from 'firebase';
+// for the default version
+import algoliasearch from 'algoliasearch';
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
+const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
 
 const db = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,7 +18,38 @@ const db = firebase.initializeApp({
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
+// Get all contacts from Firebase
+db.database().ref('/ALGOLIA_TESSTING').once('value', contacts => {
+    // Build an array of all records to push to Algolia
+    const records = [];
+    contacts.forEach(contact => {
+      // get the key and data from the snapshot
+      const childKey = contact.key;
+      const childData = contact.val();
+      console.log(childData)
+      // We set the Algolia objectID as the Firebase .key
+      childData.objectID = childKey;
+      // Add object for indexing
+      records.push(childData);
+    });
+  
+    // Add or update new objects
+    index
+      .saveObjects(records)
+      .then(() => {
+        console.log('Contacts imported into Algolia');
+      })
+      .catch(error => {
+        console.error('Error when importing contact into Algolia', error);
+        process.exit(1);
+      });
+  });
+
 // add database functions below
 
+function search() {
 
-export default db;
+}
+
+
+export default {db, client, index, search};
