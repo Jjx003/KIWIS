82e74822e51ab8dbba81f0d25c61569f64549d02
Jason Cabrera <cabrerajason7@gmail.com>
2020-05-05 17:29:52 -0700

    added container in home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
+import { Container } from 'semantic-ui-react';
+import '../css/Home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar /> 
+        <Container className="container">
+          <p> here </p>
+        </Container> 
+
       </div>
     );
   }
