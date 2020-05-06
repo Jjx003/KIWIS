8c0e7be8d27595926b435664cc419efd8f4c995d
faintj <jquong@ucsd.edu>
2020-05-06 16:26:19 -0700

    Bug Fixed

--- src/db/index.js
+++ src/db/index.js
@@ -13,5 +13,48 @@ const db = firebase.initializeApp({
 
 // add database functions below
 
+// Add new Response to Response table while updating the post table with new response
+function pushResponse(company, r_owner_id, r_upvote_count, r_post_id, r_datetime, r_content, r_endorsed){
+    
+    // Company's ref
+    const firebaseRef = db.database().ref(company);
 
-export default db;
+    var resRef = firebaseRef.child("Response").push({owner_id: r_owner_id,
+        count: r_upvote_count,
+        post_id: r_post_id,
+        datetime: r_datetime,
+        content: r_content,
+        endorsed: r_endorsed});
+
+    var ref_id = resRef.key;   
+
+    // Access posts to update
+    const postRef = db.database().ref('bruh/Posts/');     //<-- for now as there is no post child
+    
+    // Update posts with Response
+    var updates = {};
+    postRef.once('value', function(snapshot){
+        var response_array = (snapshot.val().response_ids);
+        response_array.push(ref_id);
+        updates[r_post_id] = response_array;
+        postRef.update(updates);
+    });
+    
+}
+
+// Retrieve data from a specific company based off of the ref_id
+function pullResponse(company, ref_id){
+    const firebaseRef = db.database().ref(company);
+
+    var response = {};
+    firebaseRef.child('Response/' + ref_id).once('value', function(data) {
+        data.forEach(function(child){
+            response[child.key] = child.val();
+        });
+    });
+
+    return response;
+}
+
+
+export default {db, pushResponse};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import '../css/App.css';
+import db from '../db/index.js';
 
 class Home extends React.Component {
+	pushData(){
+		var company = "bruh", r_owner_id = 111, r_upvote_count = 0, r_post_id = "-M67VDK6MLzICpgZPPPr";
+		var r_datetime = "1/11/11 1:11PM", r_content = "New Update", r_endorsed =true;
+		db.pushResponse(company, r_owner_id, r_upvote_count, r_post_id, r_datetime, r_content, r_endorsed);
+
+	}
+
 	render() {
 		return(
 			<div>
 				<h1> Home Page </h1>	
-				<Button> Hi </Button>
+				<Button onClick={this.pushData}> Hi </Button>
 			</div>
 		);
 	}
