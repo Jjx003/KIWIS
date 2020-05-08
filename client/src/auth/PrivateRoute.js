bd07668a6f4d6f73d6474a0ab56dae112df052bc
Jeff Xu / cxcharlie <jjx003@ucsd.edu>
2020-05-08 00:32:33 -0700

    Update PrivateRoute.js

--- client/src/auth/PrivateRoute.js
+++ client/src/auth/PrivateRoute.js
@@ -4,14 +4,10 @@ import { AuthContext } from "./Auth"
 
 const PrivateRoute = ({component: RouteComponent, ...rest}) => {
     const {currentUser} = useContext(AuthContext);
-    console.log(currentUser);
-    console.log("we navigating here??")
-    console.log(currentUser, " is current");
-    console.log(RouteComponent)
     return (
         <Route
             {...rest}
-            render={(routeProps) => (currentUser===true ? (<RouteComponent {...routeProps} />) : (<Redirect to="/login"/>))
+            render={(routeProps) => (currentUser ? (<RouteComponent {...routeProps} />) : (<Redirect to="/login"/>))
             }
         />
     )
