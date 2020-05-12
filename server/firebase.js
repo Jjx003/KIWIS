a542875662947a44d8b37aeb4836f1b038ab6839
Jeff Xu <jjx003@ucsd.edu>
2020-05-12 13:15:52 -0700

    Sync code, commented out

--- server/firebase.js
+++ server/firebase.js
@@ -20,5 +20,43 @@ const admin = firebase_admin.initializeApp({
   databaseURL: process.env.DATABASE_URL
 });
 
+/*
+const tableRef = db.ref('/TABLE_NAME_HERE');
+tableRef.on('child_added', addOrUpdateIndexRecord);
+tableRef.on('child_changed', addOrUpdateIndexRecord);
+tableRef.on('child_removed', deleteIndexRecord);
+
+function addOrUpdateIndexRecord(item) {
+  // Get Firebase object
+  const record = item.val();
+  // Specify Algolia's objectID using the Firebase object key
+  record.objectID = item.key;
+  // Add or update object
+  index
+    .saveObject(record)
+    .then(() => {
+      console.log('Firebase object indexed in Algolia', record.objectID);
+    })
+    .catch(error => {
+      console.error('Error when indexing contact into Algolia', error);
+      process.exit(1);
+    });
+}
+
+function deleteIndexRecord({key}) {
+  // Get Algolia's objectID from the Firebase object key
+  const objectID = key;
+  // Remove the object from Algolia
+  index
+    .deleteObject(objectID)
+    .then(() => {
+      console.log('Firebase object deleted from Algolia', objectID);
+    })
+    .catch(error => {
+      console.error('Error when deleting contact from Algolia', error);
+      process.exit(1);
+    });
+}
+*/
 
 module.exports = { db, admin };
