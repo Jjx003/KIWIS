c68bf263e22d76ae311561bea93ab311cb5fbffa
Eric Siu <esiu@ucsd.edu>
2020-05-11 23:13:37 -0700

    removed route parameters, request body only; added auth checks; editted route names for consistency

--- server/routes/tags/index.js
+++ server/routes/tags/index.js
@@ -1,53 +1,83 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../../db/index")
+var auth = require('../../auth/index');
 require('dotenv').config();
 
 
 // POST method to removes certain tag from the database and from the users of the company
-router.post('/remove/:tagName', function (req, res, next) {
+router.post('/remove', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.removeTag(req.body.forumName, req.params['tagName']);
-        db.removeTagFromUsers(req.body.forumName, req.params['tagName']);
-    } catch (error) {
-        res.send("failed");
-        console.log(error);
-        return;
-    }  
-});
+    // Checks for authorized access
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    // Removes the tag from company and users
+    function (req, res, next) {
+
+        try {
+            db.removeTag(req.body.forumName, req.body.tagName);
+            db.removeTagFromUsers(req.body.forumName, req.body.tagName);
+            res.jsonp({success: true});
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+        }  
+    }
+);
 
 // POST method to add a single tag
-router.post('/add', function (req, res, next) {
+router.post('/add', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.createNewTag(req.body.forumName, req.body.tagName);
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
 
-// Get all tags from the database
-router.get('/', function (req, res, next) {
+    function (req, res, next) {
+        try {
+            db.createNewTag(req.body.forumName, req.body.tagName);
+            res.jsonp({success: true});
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+        }  
+    }
+);
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.getTags(req.body.forumName).then((data)=>{
-            res.send(data.val());
-        });
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+// GET all tags from the database
+router.get('/all', 
+
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    function (req, res, next) {
+        try {
+            db.getTags(req.body.forumName).then((data)=>{
+                res.send(data.val());
+            });
+        } catch (error) {
+            console.log(error)
+            res.jsonp({success: false});
+        }  
+    }
+);
 
 
 
--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,67 +1,112 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../../db/index")
+var auth = require('../../auth/index');
 require('dotenv').config();
 
 // POST method to create suer to the database
-router.post('/', function (req, res, next) {
+router.post('/add', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.createNewUser(req.body.forumName, req.body.firstName, req.body.lastName, req.body.email);
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+    // Checks for authorized access
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    function (req, res, next) {
+        try {
+            db.createNewUser(req.body.forumName, req.body.firstName, req.body.lastName, req.body.email);
+            res.jsonp({success: true});
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+            return;
+        }  
+    }
+);
 
 // POST method to removes certain user from the database given a UUID
-router.post('/remove/:userID', function (req, res, next) {
+router.post('/remove', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.removeUser(req.body.forumName, req.params['userID']);
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+    // Checks for authorized access
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    function (req, res, next) {
+        try {
+            db.removeUser(req.body.forumName, req.body.userID);
+            res.jsonp({success: true});
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+            return;
+        }
+    }
+);
 
 // GET method to get a single user from the database
-router.get('/:userID', function (req, res, next) {
+router.get('/', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.getUser(req.body.forumName, req.params['userID']).then((data)=>{
-            res.send(data.val());
-        });
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+    // Checks for authorized access
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    function (req, res, next) {
+        // When moving to production, need a authentication cookie passed in as well
+        // Or else people can exploit this route.
+        try {
+            db.getUser(req.body.forumName, req.body.userID).then((data)=>{
+                res.send(data.val());
+            });
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+            return;
+        }  
+    }
+);
 
 // GET method to get all users from the database
-router.get('/all', function (req, res, next) {
+router.get('/all', 
 
-    // When moving to production, need a authentication cookie passed in as well
-    // Or else people can exploit this route.
-    try {
-        db.getUsers(req.body.forumName).then((data)=>{
-            res.send(data.val());
-        });
-    } catch (error) {
-        res.send("failed");
-        console.log(error)
-        return;
-    }  
-});
+    // Checks for authorized access
+    function (req, res, next) {
+        auth.checkToken(req.cookies.auth).then(() =>{
+            next()
+        }).catch( function(error) {
+            console.log("error occured when checking token, request denied");
+            res.jsonp({success: false});
+        })  
+    },
+
+    function (req, res, next) {
+        try {
+            db.getUsers(req.body.forumName).then((data)=>{
+                res.send(data.val());
+            });
+        } catch (error) {
+            console.log(error);
+            res.jsonp({success: false});
+            return;
+        }  
+    }
+);
 
 
 
