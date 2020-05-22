cd113e9bf1323ed0992928be4a1bab16e43ce517
Zhiqiang Pi <zhpi@ucsd.edu>
2020-05-21 17:28:52 -0700

    poop

--- client/src/routes/Settings.js
+++ client/src/routes/Settings.js
@@ -20,49 +20,51 @@ class Settings extends React.Component {
     render(){
         return(
             <div>
-            <Navbar />
-            <div className = "setting">
-                
-                <div className = "settingsWrap">
-                    <Segment basic textAlign='center'>
-                        <div className = "settings-item">                         
-                            <input className="textBox" name="email" type="email" placeholder="  abc@def.com"/>
-                        </div>
-                        <div className = "settings-item">                         
-                            <input className="textBox" name="password" type="password" placeholder="  randomPassword"/>
-                        </div>
-                        <div className = "settings-item"> 
-                            <Checkbox toggle label={{ children: 'Email Notification' }}/>
-                        </div>
-                        <div className = "settings-item"> 
-                            <Checkbox toggle label={{ children: 'Browser Notification' }}/>
-                        </div>
-                        <div className = "settings-item">
-                            <button className= "buttonz"> Submit </button>
-                        </div>
-                        <div className="instructions"> Click here to change specialization. This affects your default tags.  </div>
-
-                        <div className = "settings-item">  
-                            <button className= "buttonz">  Specialization</button>
-                        </div>
-                        <div className = "settings-item">                 
-                            <button className= "buttonz"> Sign Out </button>
-                        </div>
-
-                        <Divider horizontal> Admins Only </Divider>
-                            <div className = "settings-item"> 
-                                <Link to="/adminPage">
-                                    <button className= "buttonz" onClick={this.redirectAdmin}> Manage Forum </button>
-                                </Link>
+                <Navbar />
+                <div className = "setting">
+                    <div className = "settingsWrap">
+                        <Segment basic textAlign='center'>
+                            <div className = "settings-item">                         
+                                <input className="textBox" name="email" type="email" placeholder="  abc@def.com"/>
                             </div>
-                    </Segment>
+                            <div className = "settings-item">                         
+                                <input className="textBox" name="password" type="password" placeholder="  randomPassword"/>
+                            </div>
+                            <div className = "settings-item"> 
+                                <Checkbox toggle label={{ children: 'Email Notification' }}/>
+                            </div>
+                            <div className = "settings-item"> 
+                                <Checkbox toggle label={{ children: 'Browser Notification' }}/>
+                            </div>
+                            <div className = "settings-item">
+                                <button className= "buttonz"> Submit </button>
+                            </div>
+                            <div className="instructions"> 
+                                Click here to change specialization. This affects your default tags.  
+                            </div>
+
+                            <div className = "settings-item">
+                                <button className= "buttonz">  Specialization</button>
+                            </div>
+                            
+                            <div className = "settings-item">                 
+                                <button className= "buttonz"> Sign Out </button>
+                            </div>
+
+                            <Divider horizontal> Admins Only </Divider>
+                                <div className = "settings-item"> 
+                                    <Link to="/adminPage">
+                                        <button className= "buttonz" onClick={this.redirectAdmin}> Manage Forum </button>
+                                    </Link>
+                                </div>
+                        </Segment>
+                    </div>
+                    <div className="endText">
+                        <p1>
+                            © All Rights Reserved. KIWI by Symps.
+                        </p1>
+                    </div>
                 </div>
-                <div className="endText">
-                    <p1>
-                        © All Rights Reserved. KIWI by Symps.
-                    </p1>
-                </div>
-            </div>
             </div>
         );}
 };
