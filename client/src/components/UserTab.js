46561d80ebdf77a4d9da3aed0524e194aa8be4b4
Zhiqiang Pi <zhpi@ucsd.edu>
2020-05-21 19:52:34 -0700

    Major refactor (not really)

---
+++ client/src/components/UserTab.js
@@ -0,0 +1,58 @@
+import DisplayUser from "../components/DisplayUser"
+import { Tab, Icon } from 'semantic-ui-react'
+import React from "react";
+import axios from 'axios';
+
+class UserTab extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loading_user: true,
+            userList: ["loading"],
+            users: {"loading": true}
+        };  
+    }
+
+    componentWillMount(){
+        axios({
+            method: 'post',
+            url: 'http://localhost:9000/users/all'
+          }).then((response) => {
+              this.setState({users: response.data})
+            this.setState({userList: Object.keys(response.data),
+            loading_user: false});
+        });
+    }
+
+    render(){
+        return( <Tab.Pane className="adminPageAP"> 
+                <div className="tagUserListAP"> 
+                    {this.state.loading_user ? 
+                    <div> 
+                        <Icon loading name='spinner' /> loading...
+                    </div> : 
+                    this.state.userList.map(x => {
+                            return <div className="userItemAP"> 
+                                <DisplayUser user_id={x} 
+                                first_name={this.state.users[x]["firstName"]} 
+                                last_name={this.state.users[x]["lastName"]}
+                                email={this.state.users[x]["email"]} 
+                                admin={this.state.users[x]["admin"]}/> 
+                            </div>;
+                        })
+                    }
+                    <div className="userItemAP">
+                        <div className="addEmployeeAP"> 
+                            <div className="invitePromptAP">Add Employee Email </div>
+                            <input className="inputBoxAP" name="email" type="email" placeholder="  email" />
+                        <button> + </button>
+                        </div>
+                    </div>
+                </div> 
+            </Tab.Pane>);
+    }
+}
+
+
+export default UserTab;
--- client/src/routes/AdminPage.js
+++ client/src/routes/AdminPage.js
@@ -1,115 +1,27 @@
 import React from "react";
-import DisplayUser from "../components/DisplayUser"
-import DisplayTag from "../components/DisplayTag"
-import { Tab, Icon } from 'semantic-ui-react'
-//import users from '../dummy_data/dummy_users.json'
-//import tags from '../dummy_data/dummy_tags.json'
 import "../css/AdminPage.css"
 import Navbar from "../components/Navbar";
-import axios from 'axios';
+import UserTab from "../components/UserTab";
+import TagTab from "../components/TagTab";
+import { Tab } from 'semantic-ui-react'
+
 
 class AdminPage extends React.Component{
     constructor(props){
         super(props);
-
-        this.state = {
-            loading_user: true,
-            loading_tag: true,
-            userList: ["loading"],
-            tagList: ["loading"],
-            users: {"loading": true}
-        };  
-        
-        axios({
-            method: 'post',
-            url: 'http://localhost:9000/tags/getTags'
-          }).then((response) => {
-            this.setState({tagList: Object.keys(response.data),
-            loading_tag: false});
-        });
-
-        axios({
-            method: 'post',
-            url: 'http://localhost:9000/users/all'
-          }).then((response) => {
-              this.setState({users: response.data})
-            this.setState({userList: Object.keys(response.data),
-            loading_user: false});
-        });
     }
 
     render(){
-        
-        const handleAddTag = (event) =>{
-            event.preventDefault();
-            // should tag_ids be in line below
-            const {tagname} = event.target.elements;
-
-            console.log("click");
-            axios({
-                method: 'post',
-                url: 'http://localhost:9000/tags/add',
-                data: {
-                  tagName: tagname.value
-                }
-              }).then((response) => {
-                
-            });
-        }
-
         var panes = [
-        { menuItem: 'Users', render: () => 
-            <Tab.Pane className="adminPageAP"> 
-                <div className="tagUserListAP"> 
-                    {this.state.loading_user ? 
-                                    <div> 
-                                        <Icon loading name='spinner' /> loading 
-                                    </div> : 
-                                    this.state.userList.map(x => {
-                            return <div className="userItemAP"> 
-                                <DisplayUser user_id={x} 
-                                first_name={this.state.users[x]["firstName"]} 
-                                last_name={this.state.users[x]["lastName"]}
-                                email={this.state.users[x]["email"]} 
-                                admin={this.state.users[x]["admin"]}/> 
-                            </div>;
-                        })
-                    }
-                    <div className="userItemAP">
-                        <div className="addEmployeeAP"> 
-                            <div className="invitePromptAP">Add Employee Email </div>
-                            <input className="inputBoxAP" name="email" type="email" placeholder="  email" />
-                        <button> + </button>
-                        </div>
-                    </div>
-                </div> 
-            </Tab.Pane> },
-        { menuItem: 'Tags', render: () => 
-            <Tab.Pane className="adminPageAP" > 
-                <div className="tagListAP"> 
-                    {this.state.loading_tag ? 
-                    <div> 
-                        <Icon loading name='spinner' /> loading 
-                    </div> 
-                    : 
-                    this.state.tagList.map(x => {return<div> <DisplayTag tag_id={x}/> </div>;})}
-                </div> 
-                <div className="addTagAP"> 
-                    <div className="tagPromptAP">Add tag name </div>
-                        <form onSubmit={handleAddTag.bind(this)}>
-                            <input className="inputBoxAP" name="tagname" type="tagname" placeholder="  tag name" />
-                            <button> + </button>
-                        </form>
-                </div>
-            </Tab.Pane> 
-            }];
+            { menuItem: 'Users', render: () => <UserTab />},
+            { menuItem: 'Tags', render: () => <TagTab />}
+        ];
 
         return(
             <div>
                 <Navbar />
                 <Tab panes={panes} />
-                <div className="endText">
-                    
+                <div className="endText">                   
                     <p1>
                         © All Rights Reserved. KIWI by Symps.
                     </p1>
