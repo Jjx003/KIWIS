b2e4a400fe001a3a71d9b52095cf0208cfcbaa17
Aidan Denlinger <adenling@ucsd.edu>
2020-05-23 14:19:58 -0700

    implemented larger tags into view post page

--- src/components/OriginalPoster.js
+++ src/components/OriginalPoster.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../css/PostCards.css";
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import "../css/Forum.css"
-import DisplayingTags from "./DisplayingTags";
+import DisplayingTagsPost from "./DisplayingTagsPost";
 
 class OriginalPoster extends React.Component {
 
@@ -28,26 +28,26 @@ class OriginalPoster extends React.Component {
                 </div>
                 <div className={"postInfo"}>
                     <div>
-                    <h2 className={"postID"}>{this.state.postID}</h2>
+                        <h2 className={"postID"}>{this.state.postID}</h2>
                     </div>
                     <div className={"postBy"}>
-                    <h2>{"Posted by: " + this.state.firstName + " " + this.state.lastName}</h2>
+                        <h2>{"Posted by: " + this.state.firstName + " " + this.state.lastName}</h2>
                     </div>
                     <span>
-                    <button className={"button"}>Follow Post</button>
+                        <button className={"button"}>Follow Post</button>
                     </span>
                 </div>
                 <div className={"content"}>
                     <p className={"text-content"}>{this.state.content}</p>
                 </div>
                 <div className={"bottom-section"}>
-                <div className={"datetime"}>
-                    <h3 className={"createdDate"}>{"Created on: " + this.state.datetime}</h3>
-                    <h1 className={"karma"}><button className={"button"}>View Endorsed</button><button className={"button"}>Upvote Response</button>{"+ " + this.state.karma}</h1>
-                </div>
-                <div className={"tagList"}>
-                    <DisplayingTags tags = {this.state.tags}/>
-                </div>
+                    <div className={"datetime"}>
+                        <h3 className={"createdDate"}>{"Created on: " + this.state.datetime}</h3>
+                        <h1 className={"karma"}><button className={"button"}>View Endorsed</button><button className={"button"}>Upvote Response</button>{"+ " + this.state.karma}</h1>
+                    </div>
+                    <div className={"tagList"}>
+                        <DisplayingTagsPost tags={this.state.tags} />
+                    </div>
                 </div>
             </div>
         );
