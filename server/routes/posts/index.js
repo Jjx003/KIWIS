9667fd2fc36f4aed3e80e2d5cd0629288c5d68a8
Riley Paik <ripaik@ucsc.edu>
2020-05-23 17:49:32 -0700

    Working on frontend task

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -23,5 +23,51 @@ function (req, res) {
     });
 });
 
+router.get('/UserMadePost',
+
+function (req, res) {
+    console.log("Entered UserMadePost");
+
+    //var response_id = "-M7AyPI9Ydzq_3OvBUhu"
+    var user_id = "Rz5mBuIvkDNSwdzK3B2W8H6seQ32"
+    var post_id = "-M7AwUea2paJyqoqSW4e"
+    //var user_id = "123"
+    db.getCompanyName(user_id).then(function(snapshot) {
+        var company_name = "UXD14";
+
+        var userMadePost = db.userMadePost(company_name, user_id, post_id);
+
+        if(userMadePost == true) {
+            res.jsonp({success : true});
+        } else {
+            res.jsonp({success : false});
+        }
+
+    })
+});
+
+router.post('/UserMadePost',
+
+function (req, res) {
+    db.getCurrentUserID(req.cookies.auth).then((user_id) => {
+        db.getCompanyName(user_id).then(function(company_name) {
+            var userMadePost = db.userMadePost(company_name, user_id, post_id);
+
+            if(userMadePost == true) {
+                res.jsonp({success : pushedData});
+            } else {
+                res.jsonp({success : false});
+            }
+
+        }).catch( function(error) {
+            console.log(error);
+            res.jsonp({success: false});
+        })  
+    }).catch((error) => {
+        console.log(error);
+        console.log()
+    });
+});
+
 
 module.exports = router;
