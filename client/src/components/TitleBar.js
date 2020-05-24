5aef629896d0893c5fc0380e9e365c9ce2d486be
Aidan Denlinger <adenling@ucsd.edu>
2020-05-24 15:21:36 -0700

    updated navbar and titlebar to CSS Grids

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Menu, Image, Icon } from 'semantic-ui-react';
 import logo from '../images/logo_white.png';
 import firebase from '../auth/firebase';
 import Cookies from 'universal-cookie';
+import '../css/TitleBar.css';
 
 
 class TitleBar extends React.Component {
@@ -13,51 +14,76 @@ class TitleBar extends React.Component {
             title: this.props.title
         }
     }
-    
+
     handleSignOut = () => {
-		firebase.auth().signOut();
+        firebase.auth().signOut();
 
-		// removing cookie
-		const cookies = new Cookies();
-		cookies.remove('auth');
+        // removing cookie
+        const cookies = new Cookies();
+        cookies.remove('auth');
 
-		// redirect to home page
+        // redirect to home page
         //this.props.history.push("/login");
     }
 
     render() {
         return (
-            <div>
-                <Menu secondary size='massive' color='olive' inverted className="navbar">
-                    <Menu.Item name='KIWI'>
-                        <Link to='/'>
-                            <Image link fluid size='tiny' src={logo} />
-                        </Link>
-                        
-                    </Menu.Item>
-                    <Menu.Item bold fitted style={{ fontSize: '28px' }}>
-                            |
-                    </Menu.Item>
+            <div className="title_block">
+                <div className={"kiwiLogo"}>
+                    <img src={logo} height={'40px'} />
+                </div>
 
-                    <Menu.Item centered fitted style={{ flexGrow: 2, fontSize: '32px' }}>
-                            {this.state.title}
-                    </Menu.Item>
+                <div className={"vert-line"}>
+                    <p>|</p>
+                </div>
 
-                    <Link style={{marginTop: "4px"}} to="/login">
-                        <Menu.Item onClick={this.handleSignOut} name='sign-out'>
-                            Sign Out
-                        </Menu.Item>
-                    </Link>
+                <div className={"title"}>
+                    <p>{this.state.title}</p>
+                </div>
 
-                    <Menu.Item name='options'>
-                        <Link to='/settings'>
-                            <Icon link name="settings"/>
-                        </Link>
-                    </Menu.Item>
-                </Menu>
+                <div className={"settings"}>
+                    <Icon name="settings" size={"big"} color='grey' inverted />
+                </div>
+                <div className={"logoutButton"}>
+                    <button className={"signoutButton"} onClick={this.handleSignOut}><Icon name="sign out" size={"big"} color='grey' inverted /></button>
+                </div>
             </div>
         );
     }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <Menu secondary size='massive' color='olive' inverted className="navbar">
+    //                 <Menu.Item name='KIWI'>
+    //                     <Link to='/'>
+    //                         <Image link fluid size='tiny' src={logo} />
+    //                     </Link>
+
+    //                 </Menu.Item>
+    //                 <Menu.Item bold fitted style={{ fontSize: '28px' }}>
+    //                         |
+    //                 </Menu.Item>
+
+    //                 <Menu.Item centered fitted style={{ flexGrow: 2, fontSize: '32px' }}>
+    //                         {this.state.title}
+    //                 </Menu.Item>
+
+    //                 <Link style={{marginTop: "4px"}} to="/login">
+    //                     <Menu.Item onClick={this.handleSignOut} name='sign-out'>
+    //                         Sign Out
+    //                     </Menu.Item>
+    //                 </Link>
+
+    //                 <Menu.Item name='options'>
+    //                     <Link to='/settings'>
+    //                         <Icon link name="settings"/>
+    //                     </Link>
+    //                 </Menu.Item>
+    //             </Menu>
+    //         </div>
+    //     );
+    // }
 }
 
 export default TitleBar;
