de494eef99184442ab30c7b96d5ea37333d328b5
faintj <jquong@ucsd.edu>
2020-05-24 17:42:51 -0700

    added responses

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -4,13 +4,39 @@ var {db} = require('../../firebase')
 var {authenticated} = require('../auth/index');
 var dbIndex = require('../../db/index')
 
-router.get('/:id', (req, res, next) => {
-    db.database().ref(req.user.company + '/Posts/' + req.params.id).once('value').then((snapshot) => {
-        var posts = snapshot.val();
-        res.jsonp(posts);
-    }).catch((error) => {
-        console.log(error)
-    })
+router.get('/:id', function (req, res, next) {
+    auth.checkToken(req.cookies.auth).then(() =>{
+        next()
+    }).catch( function(error) {
+        console.log("error occured when checking token, request denied");
+        res.jsonp({success: false});
+    })  
+},
+function (req, res) {
+    getCurrentUserID(req.cookies.auth).then((user_id) => {
+        getCompanyName(user_id).then(function(company) {
+            var posts;
+            db.database().ref(company+'/Posts/'+req.params.id).once('value').then(function(snapshot) {
+                posts = snapshot.val();
+               
+           }).catch(function(error){
+            console.log(error);
+            res.jsonp({success: false});
+        })
+            pullResponse(company, req.params.id).then( (responseData) =>{
+                res.jsonp({posts: posts, responses: responseData})
+            }).catch(function(error){
+                console.log(error);
+                res.jsonp({success: false});
+            })
+        }).catch(function(error){
+            console.log(error);
+            res.jsonp({success: false});
+        })
+    })        
+    
+    
+    
 });
 
 //check auth with this get request
