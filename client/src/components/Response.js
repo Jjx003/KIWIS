f32d26c447092e1829a271616e73122b5a60eb44
Zhiqiang Pi <zhpi@ucsd.edu>
2020-05-28 13:59:04 -0700

    endorse and upvote connected to frontend, doesn't work without response id, need to talk to Aidan and Luis

--- client/src/components/Response.js
+++ client/src/components/Response.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Icon } from "semantic-ui-react";
 import "../css/Forum.css"
+import axios from 'axios';
 
 class Response extends React.Component {
 
@@ -13,11 +14,36 @@ class Response extends React.Component {
             datetime: this.props.datetime,
             karma: this.props.karma,
             content: this.props.content,
-            endorsed: this.props.endorsed
+            endorsed: this.props.endorsed,
+            response_id: 'fadfag3gw' //update this!!!
         };
     }
 
     render() {
+        const handleUpvote = () => {
+            axios({
+                method: 'post',
+                url: 'http://localhost:9000/responses/upvote',
+                data: {
+                    response_id: this.state.response_id, //Put this in!!!
+                }
+            }).then((response) => {
+                this.setState({removed: true});
+            });
+        }
+
+        const handleEndorse = () => {
+            axios({
+                method: 'post',
+                url: 'http://localhost:9000/responses/endorse',
+                data: {
+                    response_id: this.state.response_id, //Put this in!!!
+                }
+            }).then((response) => {
+                this.setState({removed: true});
+            });
+        }
+
         return (
             <div className={"response-post"}>
                 <div className={"leftSide"}>
@@ -37,9 +63,9 @@ class Response extends React.Component {
                     <div className={"rStar"}>
                         {this.state.endorsed ? <Icon name="star" color={"yellow"} size={"big"} /> : <div />}
                     </div>
-                    {this.state.firstPoster ? <div><button className={"button"}>Endorse</button></div> : <div></div>}
+                    {this.state.firstPoster ? <div><button className={"button"} onClick={handleEndorse}>Endorse</button></div> : <div></div>}
                     <div className={"rKarma"}>
-                        <h1><button className={"button"}>Upvote</button>{"   + " + this.state.karma}</h1>
+                        <h1><button className={"button"} onClick={handleUpvote}>Upvote</button>{"   + " + this.state.karma}</h1>
                     </div>
                 </div>
             </div>
