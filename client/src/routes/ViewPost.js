b484857443c5c9413dd5491cbf68220dfbbcad78
larroyor <50161629+larroyor@users.noreply.github.com>
2020-05-30 14:57:38 -0700

    Make response (#33)
    * Update
    
    * Updating responses
    
    * working response and dynamic rerender
    
    * Need to fix edge case
    
    * fix async issue with name
    
    * moved Responses to AddResponse and the components folder
    
    * fixed format
    
    * Updating
    
    * Checking deepcode fix
    
    Co-authored-by: Aidan Denlinger <adenling@ucsd.edu>

--- client/src/routes/ViewPost.js
+++ client/src/routes/ViewPost.js
@@ -6,11 +6,15 @@ import Response from "../components/Response";
 import Failure from '../components/Failure'
 import axios from 'axios'
 import TitleBar from "../components/TitleBar";
+import AddResponse from "../components/AddResponse";
 
 class ViewPost extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.latestResponse = 0;
+        this.lockResponse = false;
 
         this.state = {
             createdPost: false,
@@ -22,10 +26,21 @@ class ViewPost extends React.Component {
             tags: [],
             datetime: "",
             content: "",
+            OP: "",
             karma: 0,
             responses: [],
+            responseObjs: [],
             failed: false,
         }
+    }
+
+    getName = (userid) => {
+        let name = "User Not Found";
+        if (this.state.users !== undefined && this.state.users[userid] !== undefined) {
+            name = this.state.users[userid].firstName + " " + this.state.users[userid].lastName;
+
+        }
+        return name;
     }
 
     upvotedMerge = (arr1, arr2) => {
@@ -54,50 +69,90 @@ class ViewPost extends React.Component {
                 loaded: false,
                 failed: false,
             })
-        }).catch((error) => {
-            console.log(error);
-            this.setState({ failed: true })
-        }).then(() => {
-            axios({
-                method: 'get',
-                url: 'http://localhost:9000/users/allUsers',
-            })
-                .then((response) => {
-                    if (response.status === 200) {
-                        this.setState({ users: response.data });
-                        this.setState({ loaded: true })
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                    this.setState({ failed: true })
-                })
         })
+            .then(() => {
+                axios({
+                    method: 'get',
+                    url: 'http://localhost:9000/users/allUsers',
+                })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            this.setState({ users: response.data });
+                            console.log(response.data)
+                        }
+                    })
+                    .then(() => {
+                        var responseText = [];
+                        var responseIDs = [];
+                        if (this.state.responses) {
+                            responseText = Object.values(this.state.responses)
+                            responseIDs = Object.keys(this.state.responses)
+                        }
+
+                        this.latestResponse = responseText.length
+
+                        this.setState({
+                            responseObjs: responseText.map((obj, i) => <Response key={i} responseID={responseIDs[i]} firstPoster={this.state.createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={this.getName(obj.user_id)} />),
+                            OP: this.getName(this.state.userID),
+                            loaded: true
+                        })
+                    }).catch((error) => {
+                        console.log(error);
+                        this.setState({ failed: true })
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        this.setState({ failed: true })
+                    })
+            })
 
     }
 
+
+
+    refreshResponse = () => {
+        /* NOTE: PROBABLY MUCH BETTER WAY TO DO THIS! */
+        while (this.lockResponse) { }
+
+        this.lockResponse = true
+
+        axios({
+            method: 'get',
+            url: 'http://localhost:9000/posts/' + this.props.id.toString(),
+        }).then((results) => {
+            // do this to convince javascript that responses is an array
+            var responses = []
+            responses = results.data.responses
+            if (responses) { // now convinced that it is an array...
+                responses = Object.values(responses)
+                // cut it to the responses we haven't seen yet
+                var newResponses = responses.slice(this.latestResponse)
+
+                // update variables! offset is how many we already have, latest response is all the responses we've now read
+                var offset = this.latestResponse
+                this.latestResponse = responses.length
+
+                // update the state to have the new response objects
+                this.setState({
+                    responseObjs: this.state.responseObjs.concat(newResponses.map((obj, i) => <Response key={offset + i} firstPoster={this.state.createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={this.getName(obj.user_id)} />))
+                })
+
+                // we're done, unlock the method
+                this.lockResponse = false
+            } else {
+                console.log("no new responses")
+                this.lockResponse = false
+            }
+        }).catch((error) => {
+            console.log(error);
+            this.setState({ failed: true })
+        })
+    }
+
     render() {
-        const { createdPost, postID, userID, title, tags, datetime, content, karma, responses, loaded, failed, users } = this.state;
+        const { createdPost, postID, userID, title, tags, datetime, content, karma, responseObjs, loaded, failed, OP } = this.state;
 
         if (this.state.loaded) {
-            // Stole method from HomePosts.js
-            const getName = (userid) => {
-                let name = "User Not Found";
-                if (users !== undefined && users[userid] !== undefined) {
-                    name = users[userid].firstName + " " + users[userid].lastName;
-
-                }
-                return name;
-            }
-
-            var responseArr = []
-            var responseIDs = []
-            if (responses) {
-                responseArr = Object.values(responses)
-                responseIDs = Object.keys(responses)
-            }
-            var mapped = responseArr.map((obj, i) => <Response key={i} responseID={responseIDs[i]} userUpvoted={obj.userUpvoted} firstPoster={createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={getName(obj.user_id)} />)
-
             return (
                 <div className={"container"}>
                     <div>
@@ -105,9 +160,9 @@ class ViewPost extends React.Component {
                         <div className="posts-container">
                             <OriginalPoster firstPoster={createdPost} postID={postID} userID={userID} title={title}
                                 tags={tags} datetime={datetime} karma={karma}
-                                content={content} name={getName(userID)} />
-                            {mapped}
-                            <button className={"makeReply"}>Reply</button>
+                                content={content} name={OP} />
+                            {responseObjs}
+                            <AddResponse postID={postID} responseUpdate={this.refreshResponse.bind(this)} />
                         </div>
                     </div>
                 </div>
