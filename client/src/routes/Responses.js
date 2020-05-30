62bc0997703910f0fe02b78c708f0b78f082878d
Aidan Denlinger <adenling@ucsd.edu>
2020-05-29 17:30:59 -0700

    working response and dynamic rerender

--- client/src/routes/Responses.js
+++ client/src/routes/Responses.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 import { AuthContext, UpdateContext } from "../auth/Auth"
 import { withRouter } from 'react-router-dom';
-import {Icon} from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 
 
 class AddResponse extends React.Component {
@@ -15,30 +15,32 @@ class AddResponse extends React.Component {
     addResponse = (event) => {
 
         event.preventDefault();
-        const {responseText} = event.target.elements;
+        const { responseText } = event.target.elements;
         axios.defaults.withCredentials = true;
         axios({
-			method: 'POST',
-			url: 'http://localhost:9000/response/AddResponse',
-			data: {
+            method: 'POST',
+            url: 'http://localhost:9000/response/AddResponse',
+            data: {
                 post_id: this.props.postID,
                 content: responseText.value,
             },
             withCredentials: true
-          })
+        })
 
-          .then((response) => {
-			if (response.data.success) {
-                // Wait until update processes before redirecting
-                // Redirect to home page
-				// this.props.history.refresh(this.props.postID);
-			} else {
-				alert("Response was not created. Try again.");
-			}
-		  })
-		  .catch((error) => {
-			console.log(error);
-		  });
+            .then((response) => {
+                if (response.data.success) {
+                    // Wait until update processes before redirecting
+                    // Redirect to home page
+                    // this.props.history.refresh(this.props.postID);
+                    this.props.responseUpdate(responseText.value)
+                    responseText.value = ""
+                } else {
+                    alert("Response was not created. Try again.");
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
 
@@ -46,40 +48,40 @@ class AddResponse extends React.Component {
     render() {
         return (
             <form onSubmit={this.addResponse}>
-        <div className="AddResponse-submission">
-            {/*<form onSubmit={this.addResponse}>*/}
-            {/*    <label>*/}
-            {/*        Post*/}
-            {/*        <input name="post_id" placeholder="Post_ID" />*/}
-            {/*    </label>*/}
-            {/*    <label>*/}
-            {/*        Response Content*/}
-            {/*        <input name="content" placeholder="Response Content" />*/}
-            {/*    </label>*/}
-            {/*    <button type="Submit Response"> Submit Response </button>*/}
-            {/*</form>*/}
-            {/*<form onSubmit={this.pullResponse}>*/}
-            {/*    <label>*/}
-            {/*        Post_ID*/}
-            {/*        <input name = "post_id1" placeholder = "POST_ID" />*/}
-            {/*    </label>*/}
-            {/*    <button type="Pull response"> Pull responses</button>*/}
-            {/*</form>*/}
-            {/*nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn*/}
-            <div className={"writingReply"}>
-                <div className={"leftSide"}>
-                    <div className={"rPoster"}>
-                        <h2 className={"responder"}>{"Gary" + ": "}</h2>
-                    </div>
-                    <div>
-                        <button className={"button"}>Post Response</button>
+                <div className="AddResponse-submission">
+                    {/*<form onSubmit={this.addResponse}>*/}
+                    {/*    <label>*/}
+                    {/*        Post*/}
+                    {/*        <input name="post_id" placeholder="Post_ID" />*/}
+                    {/*    </label>*/}
+                    {/*    <label>*/}
+                    {/*        Response Content*/}
+                    {/*        <input name="content" placeholder="Response Content" />*/}
+                    {/*    </label>*/}
+                    {/*    <button type="Submit Response"> Submit Response </button>*/}
+                    {/*</form>*/}
+                    {/*<form onSubmit={this.pullResponse}>*/}
+                    {/*    <label>*/}
+                    {/*        Post_ID*/}
+                    {/*        <input name = "post_id1" placeholder = "POST_ID" />*/}
+                    {/*    </label>*/}
+                    {/*    <button type="Pull response"> Pull responses</button>*/}
+                    {/*</form>*/}
+                    {/*nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn*/}
+                    <div className={"writingReply"}>
+                        <div className={"leftSide"}>
+                            <div className={"rPoster"}>
+                                <h2 className={"responder"}>{"Gary" + ": "}</h2>
+                            </div>
+                            <div>
+                                <button className={"button"}>Post Response</button>
+                            </div>
+                        </div>
+
+                        <textarea name={"responseText"} placeholder={"Write your response here."} />
                     </div>
                 </div>
-
-                <textarea name={"responseText"} placeholder={"Write your response here."}/>
-            </div>
-        </div>
-                </form>
+            </form>
         );
 
     }
--- client/src/routes/ViewPost.js
+++ client/src/routes/ViewPost.js
@@ -13,6 +13,8 @@ class ViewPost extends React.Component {
     constructor(props) {
         super(props);
 
+        this.latestResponse = 0;
+
         this.state = {
             createdPost: false,
             loaded: false,
@@ -23,10 +25,22 @@ class ViewPost extends React.Component {
             tags: [],
             datetime: "",
             content: "",
+            OP: "",
             karma: 0,
-            responses: [],
+            responseIDs: [],
+            responseObjs: [],
             failed: false,
         }
+    }
+
+    getName = (userid) => {
+        let name = "User Not Found";
+        const users = this.state.users;
+        if (users !== undefined && users[userid] !== undefined) {
+            name = users[userid].firstName + " " + users[userid].lastName;
+
+        }
+        return name;
     }
 
     componentDidMount() {
@@ -42,7 +56,7 @@ class ViewPost extends React.Component {
                 tags: results.data.posts.tag_ids,
                 datetime: results.data.posts.date_time,
                 karma: results.data.posts.karma,
-                responses: results.data.responses,
+                responseIDs: results.data.responses,
                 content: results.data.posts.content,
                 userID: results.data.posts.user_id,
                 loaded: false,
@@ -59,40 +73,65 @@ class ViewPost extends React.Component {
                 .then((response) => {
                     if (response.status === 200) {
                         this.setState({ users: response.data });
-                        this.setState({ loaded: true })
                     }
                 })
                 .catch((error) => {
                     console.log(error);
                     this.setState({ failed: true })
                 })
+        }).then(() => {
+            // Stole method from HomePosts.js
+            var responseText = [];
+            if (this.state.responseIDs) {
+                responseText = Object.values(this.state.responseIDs)
+            }
+
+            this.latestResponse = responseText.length
+
+            this.setState({
+                responseObjs: responseText.map((obj, i) => <Response key={i} firstPoster={this.state.createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={this.getName(obj.user_id)} />),
+                OP: this.getName(this.state.userID),
+                loaded: true
+            })
         })
 
     }
 
+
+
+    refreshResponse = () => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:9000/posts/' + this.props.id.toString(),
+        }).then((results) => {
+            // do this to convince javascript that responses is an array
+            var newResponses = []
+            newResponses = results.data.responses
+            if (newResponses) { // now convinced that it is an array...
+                newResponses = Object.values(newResponses)
+                // cut it to the responses we haven't seen yet
+                newResponses = newResponses.slice(this.latestResponse)
+
+                // update the state to have the new response objects
+                this.setState({
+                    responseObjs: this.state.responseObjs.concat(newResponses.map((obj, i) => <Response key={this.latestResponse + i} firstPoster={this.state.createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={this.getName(obj.user_id)} />))
+                })
+                // update latest response to show we've read new responses
+                this.latestResponse = results.data.responses.length
+            } else {
+                console.log("no new responses")
+            }
+
+        }).catch((error) => {
+            console.log(error);
+            this.setState({ failed: true })
+        })
+    }
+
     render() {
-        const { createdPost, postID, userID, title, tags, datetime, content, karma, responses, loaded, failed, users } = this.state;
+        const { createdPost, postID, userID, title, tags, datetime, content, karma, responseObjs, loaded, failed, OP } = this.state;
 
         if (this.state.loaded) {
-            // Stole method from HomePosts.js
-            const getName = (userid) => {
-                let name = "User Not Found";
-                if (users !== undefined && users[userid] !== undefined) {
-                    name = users[userid].firstName + " " + users[userid].lastName;
-
-                }
-                return name;
-            }
-
-            var responseArr = []
-            if (responses) {
-                responseArr = Object.values(responses)
-                console.log(responseArr)
-            }
-            var mapped = responseArr.map((obj, i) => <Response key={i} firstPoster={createdPost} datetime={obj.datetime} content={obj.content} karma={obj.karma} endorsed={obj.endorsed} name={getName(obj.user_id)} />)
-
-
-
             return (
                 <div className={"container"}>
                     <div>
@@ -100,9 +139,9 @@ class ViewPost extends React.Component {
                         <div className="posts-container">
                             <OriginalPoster firstPoster={createdPost} postID={postID} userID={userID} title={title}
                                 tags={tags} datetime={datetime} karma={karma}
-                                content={content} name={getName(userID)} />
-                            {mapped}
-                            <AddResponse postID={postID}/>
+                                content={content} name={OP} />
+                            {responseObjs}
+                            <AddResponse postID={postID} responseUpdate={this.refreshResponse.bind(this)} />
                         </div>
                     </div>
                 </div>
