d09d7c0d9f6f21626d5213a625b1f96771d76ba6
Jeff Xu <jjx003@ucsd.edu>
2020-06-02 15:16:35 -0700

    fix

---
+++ client/build/precache-manifest.354fc809590da62e0691ca31154db76b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972e464b8e0972ec025b72dfb88871f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1659cf4130201789b3e",
+    "url": "/static/css/main.acd4a19e.chunk.css"
+  },
+  {
+    "revision": "7b5e4716d26b2cc02a5a",
+    "url": "/static/js/2.863c1c93.chunk.js"
+  },
+  {
+    "revision": "595674c894a58c29d63655209a2c56e7",
+    "url": "/static/js/2.863c1c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1659cf4130201789b3e",
+    "url": "/static/js/main.34170752.chunk.js"
+  },
+  {
+    "revision": "2596174d18c365c6b067",
+    "url": "/static/js/runtime-main.48825a9d.js"
+  },
+  {
+    "revision": "b50a6676ae3e236dc36bee4ba935202d",
+    "url": "/static/media/logo_white.b50a6676.png"
+  },
+  {
+    "revision": "42df0b1faa6397b7c4309a6a8cc2d96d",
+    "url": "/static/media/vectorlogo.42df0b1f.png"
+  }
+]);
