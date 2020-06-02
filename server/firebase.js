3067ed2b9d7ae999e32b00f1c981a4a55b8e69c1
Jeff Xu <jjx003@ucsd.edu>
2020-06-02 16:34:08 -0700

    config

--- server/firebase.js
+++ server/firebase.js
@@ -17,7 +17,20 @@ const db = firebase_init.initializeApp({
 // used for generating tokens
 var serviceAccount = require("./serviceKey.json");
 const admin = firebase_admin.initializeApp({
-  credential: firebase_admin.credential.cert(serviceAccount),
+  //credential: firebase_admin.credential.cert(serviceAccount),
+  //credential:firebase_admin.credential.applicationDefault(),
+  credential: firebase_admin.credential.cert({
+    type:"service_account",
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
+  }),
   databaseURL: process.env.DATABASE_URL
 });
 
