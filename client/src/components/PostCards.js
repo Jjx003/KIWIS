b8e67b946f91ddad1d49131231753542def9f63f
Luis <larroyor@ucsd.edu>
2020-06-02 18:16:27 -0700

    Reworked postcards

--- client/src/components/PostCards.js
+++ client/src/components/PostCards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../css/PostCards.css";
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import DisplayingTags from "./DisplayingTags";
 
 class PostCards extends React.Component {
@@ -11,21 +11,26 @@ class PostCards extends React.Component {
     handleClick() {
         const {history} = this.props;
         history.push({      //change to replace and add back button
-            pathname: '/viewPost/'+this.props.post_id.toString(), 
+            pathname: '/viewPost/' + this.props.post_id.toString(),
         });
     }
-    
-    render() {
-        return(
-            <div onClick={this.handleClick.bind(this)} className="post-cards">
-                <h1 className="PostTitle"> {this.props.title} </h1>
-                <div className="PostTags">
-                    <DisplayingTags tags={this.props.tag_ids} />
-                </div>
-                <h3 className="Poster"> {"Created by: " + this.props.name} </h3>
-                <h3 className="PostDate"> {"Date Created: " + this.props.date_time} </h3>
 
-            </div >
+    render() {
+        return (
+            <div onClick={this.handleClick.bind(this)} className="post-cards">
+                <div className={"PostTitle"}>
+                    <h1> {this.props.title} </h1>
+                </div>
+                <div className={"Poster"}>
+                    <h3> {"Created by: " + this.props.name} </h3>
+                </div>
+                <div className="PostTags">
+                    <DisplayingTags tags={this.props.tag_ids}/>
+                </div>
+                <div className={"PostDate"}>
+                    <h3> {"Date Created: " + this.props.date_time} </h3>
+                </div>
+            </div>
         );
     }
 }
