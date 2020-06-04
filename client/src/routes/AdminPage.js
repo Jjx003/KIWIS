2932a00e1b50aab0fbc3b1090933443029129cf7
Aidan Denlinger <adenling@ucsd.edu>
2020-06-03 17:56:11 -0700

    removed all unneeded constructors

--- client/src/routes/AdminPage.js
+++ client/src/routes/AdminPage.js
@@ -8,24 +8,21 @@ import TagTab from "../components/TagTab";
 import { Tab } from 'semantic-ui-react'
 
 
-class AdminPage extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class AdminPage extends React.Component {
 
-    render(){
+    render() {
         var panes = [
-            { menuItem: 'Users', render: () => <UserTab />},
-            { menuItem: 'Tags', render: () => <TagTab />},
-            { menuItem: 'Metadata - Tags', render: () => <MetadataTagTab  />},
-            { menuItem: 'Metadata - Users', render: () => <MetadataUserTab />}
+            { menuItem: 'Users', render: () => <UserTab /> },
+            { menuItem: 'Tags', render: () => <TagTab /> },
+            { menuItem: 'Metadata - Tags', render: () => <MetadataTagTab /> },
+            { menuItem: 'Metadata - Users', render: () => <MetadataUserTab /> }
         ];
 
-        return(
+        return (
             <div>
                 <TitleBar title="Manage Forum" />
                 <Tab panes={panes} />
-                <div className="endText">                   
+                <div className="endText">
                     <p1>
                         © All Rights Reserved. KIWI by Symps.
                     </p1>
